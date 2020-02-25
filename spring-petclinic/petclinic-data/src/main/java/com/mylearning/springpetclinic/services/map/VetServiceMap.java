588c59878a3b335f1ab61a3dee60d76a2b4d1292
Sandeep Kumar <rsandeepm2512@gmail.com>
2020-02-25 22:43:27 +0530

    Implementing Map based services. Closes #6

---
+++ spring-petclinic/petclinic-data/src/main/java/com/mylearning/springpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package com.mylearning.springpetclinic.services.map;
+
+import java.util.Set;
+
+import com.mylearning.springpetclinic.model.Vet;
+import com.mylearning.springpetclinic.services.CrudService;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+	@Override
+	public Set<Vet> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Vet findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Vet save(Vet object) {
+		return super.save(object.getId(), object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Vet object) {
+		super.delete(object);
+	}
+}
