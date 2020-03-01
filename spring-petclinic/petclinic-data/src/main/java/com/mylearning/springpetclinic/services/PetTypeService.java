c5c32bc812928ad46a985a940652ab8c690cce26
Sandeep Kumar <rsandeepm2512@gmail.com>
2020-03-01 10:31:47 +0530

    Added PetType Service. Closes #29

---
+++ spring-petclinic/petclinic-data/src/main/java/com/mylearning/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.mylearning.springpetclinic.services;
+
+import com.mylearning.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ spring-petclinic/petclinic-data/src/main/java/com/mylearning/springpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,39 @@
+package com.mylearning.springpetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.mylearning.springpetclinic.model.PetType;
+import com.mylearning.springpetclinic.services.PetTypeService;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+	@Override
+	public Set<PetType> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public PetType findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public PetType save(PetType object) {
+		return super.save(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(PetType object) {
+		super.delete(object);
+	}
+
+	
+}
